a60c9de84316d15c5869d294e39a2c2398c72ec7
yurinanakazato <47862935+yurinanakazato@users.noreply.github.com>
2025-01-31 12:48:29 +0000

    Deploying to gh-pages from @ yurinanakazato/yurinanakazato.github.io@061a7781bc270b19fcf0a48d0eac4b5a4b22fb2a 🚀

--- ja-jp/assets/js/search-data.js
+++ ja-jp/assets/js/search-data.js
@@ -35,7 +35,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/talks/";
+            window.location.href = "/leadership/";
           },
         },{id: "post-a-post-with-image-galleries",
       
