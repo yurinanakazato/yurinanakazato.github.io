5345201412ef082cca0e4eee318d0ad1f41113e2
yurinanakazato <47862935+yurinanakazato@users.noreply.github.com>
2025-01-31 13:28:45 +0000

    Deploying to gh-pages from @ yurinanakazato/yurinanakazato.github.io@66cd76e9014e08984f2f11a0706edb3febef0b07 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,35 +433,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%79%75%72%69%6E%61.%6E%61%6B%61%7A%61%74%6F@%70%68%79%73.%73.%75-%74%6F%6B%79%6F.%61%63.%6A%70", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/yurinanakazato0207", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://orcid.org/0000-0002-0984-7713", "_blank");
         },
       },{
       id: 'light-theme',
