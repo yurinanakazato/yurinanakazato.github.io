512dd8f5d177dd6e77f93dcebe85b2074dafc765
yurinanakazato <47862935+yurinanakazato@users.noreply.github.com>
2025-12-24 16:13:04 +0000

    Deploying to gh-pages from @ yurinanakazato/yurinanakazato.github.io@9b1e1128696d494b680755d50d5af1a40f958874 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-talks",
           title: "talks",
-          description: "",
+          description: "* indicates invited talks",
           section: "Navigation",
           handler: () => {
             window.location.href = "/talks/";
